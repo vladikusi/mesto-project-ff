5754e088fc1751c29f8dbdef63a459d0703fafcb
vladikusi <vladikusi920@gmail.com>
2026-03-06 03:26:08 +0300

    Написан код по заданию к проекту

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const placeCardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узлы
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createPlaceCard (cardData, onDelete) {
+  const placeCardElement = placeCardTemplate.querySelector('.places__item').cloneNode(true);
+  const title = placeCardElement.querySelector('.card__title');
+  const image = placeCardElement.querySelector('.card__image');
+  const deleteButton = placeCardElement.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+  title.textContent = cardData.name;
+  image.src = cardData.link;
+  deleteButton.addEventListener('click', onDelete);
+  
+  return placeCardElement;
+}
 
-// @todo: Вывести карточки на страницу
+// Функция удаления карточки
+function deletePlaceCard(evt) {
+  const placeCard = evt.target.closest('.places__item');
+  placeCard.remove(); 
+}
+
+// Вывод карточек на страницу
+initialCards.forEach((card) => {
+  const placeCard = createPlaceCard(card, deletePlaceCard);
+  placesList.append(placeCard);
+})
+
